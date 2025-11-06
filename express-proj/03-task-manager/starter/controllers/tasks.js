f15afe16004370eb5f7fce8a7565258cdc5eba69
Shahid0019 <SHAH.SHAHID0019@GMAIL.COM>
2025-11-06 23:58:15 +0530

    push

--- express-proj/03-task-manager/starter/controllers/tasks.js
+++ express-proj/03-task-manager/starter/controllers/tasks.js
@@ -1,6 +1,11 @@
 const Task = require("../models/task");
-const getAllTasks = (req, res) => {
-  res.send("all items");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
@@ -10,16 +15,45 @@ const createTask = async (req, res) => {
   } catch (error) {
     res.status(500).json({ mgs: error });
   }
-}; 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
 };
-const updateTask = (req, res) => {
-  res.send("task updated");
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body);
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("task deleted");
+const deleteTask = async (req, res) => {
+  try {
+    res.send("task deleted");
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id :${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
